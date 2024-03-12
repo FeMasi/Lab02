6335db5f58e43302e689465126a458579a6fddb8
fede <federico.masiello97@gmail.com>
2024-03-12 15:19:12 +0100

    ora scrive su file

--- main.py
+++ main.py
@@ -6,23 +6,39 @@ t.loadDictionary(file_path)
 
 while(True):
 
-
     t.printMenu()
 
-
+    caratteri = False
 
     txtIn = input("seleziona un opzione: ")
 
     # Add input control here!
+    if (txtIn.isnumeric()):
+        caratteri = True
+    else:
+        caratteri = False
+        print("input non valido")
+        continue
 
     if int(txtIn) == 1:
         print("inserisci la parola da aggiungere nel formato <parola aliena> <traduzione>:")
         query = input()
+        if (query.isalpha()):
+            caratteri = True
+        else:
+            caratteri = False
+            print("input non valido")
+            continue
         t.handleAdd(query)
 
     if int(txtIn) == 2:
-
         parola_aliena = input("Inserisci la parola aliena da tradurre: ").lower()
+        if (parola_aliena.isalpha()):
+            caratteri = True
+        else:
+            caratteri = False
+            print("input non valido")
+            continue
         t.handleTranslate(parola_aliena)
 
     if int(txtIn) == 3:
